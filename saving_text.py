363111796c070b4756e2ada095334d48fcbd9880
sarath k <85940310+sarathravik@users.noreply.github.com>
2023-04-12 14:55:34 +0530

    updatd location

--- saving_text.py
+++ saving_text.py
@@ -1,5 +1,5 @@
 text='jenkin pipline creation'
-with open(r'C:\Mitz\RFP\rfpijenkin pipiline code-test\rfptest\newfile.txt', 'w') as file:
+with open(r'C:\Mitz\RFP\rfpijenkin pipiline code-test\rfptest\jenkins\newfile.txt', 'w') as file:
     # Write the data from the original file to the new file
     file.write(text)
 
